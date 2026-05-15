7527808ff7390b912839c25d4c9aecec782e56f8
Jacob Cobti <cobtijacob@gmail.com>
2026-05-15 08:33:35 -0700

    New Update for my project

--- scripts/index.js
+++ scripts/index.js
@@ -33,10 +33,25 @@ const initialCards = [
 const editProfileBtn = document.querySelector(".profile__edit-btn");
 const editProfileModal = document.querySelector("#edit-profile-modal");
 const editProfileCloseBtn = editProfileModal.querySelector(".modal__close-btn");
+const editProfileFormEl = editProfileModal.querySelector(".modal__form");
+const editProfileNameInput = editProfileModal.querySelector(
+  "#profile-name-input",
+);
+const editProfileDescriptionInput = editProfileModal.querySelector(
+  "#profile-description-input",
+);
 
 const newPostBtn = document.querySelector(".profile__add-btn");
 const newPostModal = document.querySelector("#new-post-modal");
 const newPostCloseBtn = newPostModal.querySelector(".modal__close-btn");
+const newPostFormEl = newPostModal.querySelector(".modal__form");
+const newPostNameInput = newPostModal.querySelector("#post-image-link-input");
+const newPostDescriptionInput = newPostModal.querySelector(
+  "#post-caption-input",
+);
+
+const profileNameEl = document.querySelector(".profile__name");
+const profileDescriptionEl = document.querySelector(".profile__description");
 
 function openModal(modal) {
   modal.classList.add("modal_is-opened");
@@ -48,6 +63,8 @@ function closeModal(modal) {
 
 editProfileBtn.addEventListener("click", function () {
   openModal(editProfileModal);
+  editProfileNameInput.value = profileNameEl.textContent;
+  editProfileDescriptionInput.value = profileDescriptionEl.textContent;
 });
 
 editProfileCloseBtn.addEventListener("click", function () {
@@ -66,3 +83,21 @@ initialCards.forEach(function (card) {
   console.log(card.name);
   console.log(card.link);
 });
+
+function handleProfileFormSubmit(evt) {
+  evt.preventDefault();
+
+  profileNameEl.textContent = editProfileNameInput.value;
+  profileDescriptionEl.textContent = editProfileDescriptionInput.value;
+
+  editProfileModal.classList.remove("modal_is-opened");
+}
+
+function handleAddCardSubmit(evt) {
+  evt.preventDefault();
+
+  newPostImageLinkEl.textContent = newPostImageLinkInput.value;
+  newPostCaptionEl.textContent = newPostCaptionInput.value;
+
+  newPostModal.classList.remove("modal_is-opened");
+}
